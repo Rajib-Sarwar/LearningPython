9aa70051c783cbca879e5940338f79aa3827cb0d
Ayon Chowdhury <md.rajib.sarwar@gmail.com>
2022-02-06 04:04:52 -0500

    overrriding variables, using collection types and use of local and global variables

--- Ch2 - Basics/variables_start.py
+++ Ch2 - Basics/variables_start.py
@@ -1,6 +1,8 @@
 # 
 # Example file for variables
+# LinkedIn Learning Python course by Joe Marini
 #
+
 
 # Basic data types in Python: Numbers, Strings, Booleans, Sequences, Dictionaries
 myint = 5
@@ -19,18 +21,34 @@ print(mylist)
 print(mytuple)
 print(mydict)
 
-
 # re-declaring a variable works
+myint = "abc"
+print (myint)
 
 # to access a member of a sequence type, use []
-
+print(mylist[2])
+print(mytuple[1])
 # use slices to get parts of a sequence
-
+print(mylist[1:4:2])
 # you can use slices to reverse a sequence
+print(mylist[::-1])
 
 # dictionaries are accessed via keys
+print(mydict["one"]) 
 
 # ERROR: variables of different types cannot be combined
+#print ("string type " + 123)
+print ("string type " + str(123))
 
 # Global vs. local variables in functions
+def someFunction():
+    #global mystr
+    mystr = "def"
+    print (mystr)
+
+someFunction()
+print (mystr) 
+
+del mystr
+print (mystr)
 
